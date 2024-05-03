1f23e89914ce3a68c8698aa141b1aa26d1f3d613
Ivozeraa <joseivo2008@gmail.com>
2024-05-03 20:44:14 -0300

    Alterações - 03/05

--- js/index.js
+++ js/index.js
@@ -74,4 +74,10 @@ window.onload = function() {
 
     // Redimensiona o canvas no carregamento da página
     resizeCanvas();
+
+    // Adicione um evento de clique ao botão do menu de hambúrguer
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.nav-ul').classList.toggle('active');
+});
+
 };
